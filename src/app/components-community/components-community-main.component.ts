5081c113ba42877b43d38415e2061c4900367b8f
Facundo Crego <facundo.crego@gmail.com>
2017-11-07 20:12:38 -0300

    Added autocomplete component

--- src/app/components-community/components-community-main.component.ts
+++ src/app/components-community/components-community-main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CompleterService, CompleterData } from 'ng2-completer';
 
 @Component({
   selector: 'app-components-community-main',
@@ -7,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComponentsCommunityMainComponent implements OnInit {
 
-  constructor() { }
+  protected searchStr: string;
+  protected captain: string;
+  protected dataService: CompleterData;
+  protected searchData = [
+    { country: 'Argentina', value: 'Argentina' },
+    { country: 'Brazil', value: 'Brazil' },
+    { country: 'Croatia', value: 'Croatia' },
+    { country: 'Denmark', value: 'Denmark' },
+    { country: 'France', value: 'France' },
+    { country: 'Holland', value: 'Holland' },
+    { country: 'Poland', value: 'Poland' },
+    { country: 'United States', value: 'United States' }
+  ];
+  protected captains = ['James T. Kirk', 'Benjamin Sisko', 'Jean-Luc Picard', 'Spock', 'Jonathan Archer', 'Hikaru Sulu', 'Christopher Pike', 'Rachel Garrett' ];
+
+  constructor(private completerService: CompleterService) {
+    this.dataService = completerService.local(this.searchData, 'country', 'country');
+  }
 
   ngOnInit() {
   }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ColorPickerModule } from 'ngx-color-picker';
+import { Ng2CompleterModule } from "ng2-completer";
 
 import { MatGridListModule } from '@angular/material';
 import { MatChipsModule } from '@angular/material';
@@ -9,16 +11,20 @@ import { MatToolbarModule } from '@angular/material';
 
 @NgModule({
   imports: [
+    FormsModule,
     MatGridListModule,
     MatChipsModule,
     MatToolbarModule,
-    ColorPickerModule
+    ColorPickerModule,
+    Ng2CompleterModule
   ],
   exports: [
+    FormsModule,
     MatGridListModule,
     MatChipsModule,
     MatToolbarModule,
-    ColorPickerModule
+    ColorPickerModule,
+    Ng2CompleterModule
   ]
 })
 export class MaterialDesignWrapperModule { } 
